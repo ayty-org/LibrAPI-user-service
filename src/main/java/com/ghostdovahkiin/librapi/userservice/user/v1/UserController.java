76febefeb52e66e7a3a94ff142dbc0db2a02d443
Pedro Henrique <pedro.sousa@dcx.ufpb.br>
2021-03-01 12:26:23 -0300

    :fire: feat: UserController

---
+++ src/main/java/com/ghostdovahkiin/librapi/userservice/user/v1/UserController.java
@@ -0,0 +1,83 @@
+package com.ghostdovahkiin.librapi.userservice.user.v1;
+
+import com.ghostdovahkiin.librapi.userservice.user.User;
+import com.ghostdovahkiin.librapi.userservice.user.UserDTO;
+import com.ghostdovahkiin.librapi.userservice.user.services.DeleteUserService;
+import com.ghostdovahkiin.librapi.userservice.user.services.GetUserBySpecificId;
+import com.ghostdovahkiin.librapi.userservice.user.services.GetUserService;
+import com.ghostdovahkiin.librapi.userservice.user.services.ListPageUserService;
+import com.ghostdovahkiin.librapi.userservice.user.services.ListUserService;
+import com.ghostdovahkiin.librapi.userservice.user.services.SaveUserService;
+import com.ghostdovahkiin.librapi.userservice.user.services.UpdateUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/v1/api/users")
+public class UserController {
+
+    private final ListUserService listUserService;
+    private final GetUserService getUserService;
+    private final SaveUserService saveUserService;
+    private final DeleteUserService deleteUserService;
+    private final UpdateUserService updateUserService;
+    private final ListPageUserService listPageUserService;
+    private final GetUserBySpecificId getUserBySpecificId;
+
+    @GetMapping(path = "/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<UserDTO> findAll() {
+        return UserDTO.fromAll(listUserService.findAll());
+    }
+
+    @GetMapping(value = "/id/{uuid}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO findBySpecificID(@PathVariable String uuid) {
+        return UserDTO.from(getUserBySpecificId.findBySpecificId(uuid));
+    }
+
+    @GetMapping(path = "/")
+    @ResponseStatus(HttpStatus.OK)
+    public Page<UserDTO> listPageUser(Pageable pageable) {
+        return UserDTO.fromPage(listPageUserService.listPages(pageable));
+    }
+
+    @GetMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserDTO findByID(@PathVariable Long id) {
+        return UserDTO.from(getUserService.findById(id));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@Valid @RequestBody UserDTO userDTO){
+        saveUserService.save(User.to(userDTO));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        deleteUserService.delete(id);
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@Valid @RequestBody UserDTO userDTO, @PathVariable Long id){
+        updateUserService.update(userDTO, id);
+    }
+}
